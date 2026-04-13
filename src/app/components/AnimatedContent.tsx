d8103697349ba2174cd2446db51f75106f19e82f
kako-jun <3541096+kako-jun@users.noreply.github.com>
2026-04-13 21:04:58 +0900

    fix: #35 原文表示時にフッタ下に余白が出るレイアウト崩れを修正
    AnimatedContentのvisibility:hidden+absoluteによる高さ確保を
    CSS Grid重ね合わせに変更。両コンテンツが同一grid-areaに配置され、
    コンテナ高さは常に両者の最大値になるため、表示切替時の
    高さ縮小によるフッタ下余白が発生しなくなる。

--- src/app/components/AnimatedContent.tsx
+++ src/app/components/AnimatedContent.tsx
@@ -1,58 +1,45 @@
-'use client'
+'use client';
 
 interface AnimatedContentProps {
-  originalContent: React.ReactNode
-  osakaContent: React.ReactNode
-  showOsaka: boolean
-  className?: string
+  originalContent: React.ReactNode;
+  osakaContent: React.ReactNode;
+  showOsaka: boolean;
+  className?: string;
 }
 
-export const AnimatedContent = ({ 
-  originalContent, 
-  osakaContent, 
-  showOsaka, 
-  className = '' 
+export const AnimatedContent = ({
+  originalContent,
+  osakaContent,
+  showOsaka,
+  className = '',
 }: AnimatedContentProps) => {
   return (
-    <div className={`relative ${className}`}>
-      {/* 高さ確保用の非表示要素 */}
-      <div style={{ visibility: 'hidden', position: 'relative' }}>
-        {showOsaka ? osakaContent : originalContent}
-      </div>
-      
+    <div className={`grid ${className}`} style={{ gridTemplateAreas: '"content"' }}>
       {/* 原文コンテンツ */}
       <div
         className="transition-opacity duration-500 ease-in-out"
-        style={{ 
+        style={{
+          gridArea: 'content',
           opacity: showOsaka ? 0 : 1,
-          position: 'absolute',
-          top: '0px',
-          left: '0px',
-          right: '0px',
-          margin: 0,
-          padding: 0,
-          pointerEvents: showOsaka ? 'none' : 'auto'
+          pointerEvents: showOsaka ? 'none' : 'auto',
         }}
+        aria-hidden={showOsaka}
       >
         {originalContent}
       </div>
-      
+
       {/* 大阪弁コンテンツ */}
       <div
         className="transition-opacity duration-500 ease-in-out"
-        style={{ 
+        style={{
+          gridArea: 'content',
           opacity: showOsaka ? 1 : 0,
-          position: 'absolute',
-          top: '0px',
-          left: '0px',
-          right: '0px',
-          margin: 0,
-          padding: 0,
-          pointerEvents: showOsaka ? 'auto' : 'none'
+          pointerEvents: showOsaka ? 'auto' : 'none',
         }}
+        aria-hidden={!showOsaka}
       >
         {osakaContent}
       </div>
     </div>
-  )
-}
+  );
+};
