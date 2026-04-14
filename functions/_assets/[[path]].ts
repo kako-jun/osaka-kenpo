b84e276d480a04872773d19ce40c7ca08c144f5f
kako-jun <3541096+kako-jun@users.noreply.github.com>
2026-04-14 16:43:40 +0900

    feat: arrivalsランディングをNext.js rewritesでプロキシ（Functions方式から変更）

--- functions/_assets/[[path]].ts
+++
@@ -1,5 +0,0 @@
-export const onRequest: PagesFunction = async ({ params }) => {
-  const path = (params.path as string[]).join('/');
-  const res = await fetch(`https://arrivals.llll-ll.com/_assets/${path}`);
-  return new Response(res.body, { status: res.status, headers: res.headers });
-};
