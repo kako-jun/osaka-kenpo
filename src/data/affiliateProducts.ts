b35403f91ba194dfb802ac642db092aae162e265
kako-jun <3541096+kako-jun@users.noreply.github.com>
2026-06-08 23:24:51 +0900

    Use high resolution affiliate images

--- src/data/affiliateProducts.ts
+++ src/data/affiliateProducts.ts
@@ -2,7 +2,8 @@
 //
 // アソシエイト ID は kako-jun の `ultimate-battle-22`。
 // amzn.to 短縮リンクを使用 (Associates ダッシュボードで生成)。
-// 画像 URL は `https://m.media-amazon.com/images/P/{ASIN}.01._SL1500_.jpg` 形式。
+// 画像 URL は商品ページのギャラリーにある `images/I/{imageId}._SL1500_.jpg` を使用。
+// ASIN 直指定の `images/P/{ASIN}.01...` は低解像度になることがある。
 
 export interface AffiliateProduct {
   /** amzn.to 短縮 URL */
@@ -19,19 +20,19 @@ export const AFFILIATE_PRODUCTS: AffiliateProduct[] = [
   {
     url: 'https://amzn.to/4ufazjL',
     title: 'よつばと! 16巻',
-    imageUrl: 'https://m.media-amazon.com/images/P/404916194X.01._SL1500_.jpg',
+    imageUrl: 'https://m.media-amazon.com/images/I/81Fj5V8VcZL._SL1500_.jpg',
     caption: '大阪先生を要チェックや！',
   },
   {
     url: 'https://amzn.to/3Sqt4EE',
     title: 'あずまんが大王1年生 新装版',
-    imageUrl: 'https://m.media-amazon.com/images/P/B0DYNRNC4Y.01._SL1500_.jpg',
+    imageUrl: 'https://m.media-amazon.com/images/I/512mu1PgoYL._SL1500_.jpg',
     caption: 'いや、原典……！',
   },
   {
     url: 'https://amzn.to/4vF4bDQ',
     title: 'こども六法 第2版',
-    imageUrl: 'https://m.media-amazon.com/images/P/433535990X.01._SL1500_.jpg',
+    imageUrl: 'https://m.media-amazon.com/images/I/81k3e3QrLxL._SL1500_.jpg',
     caption: 'いじめ防止対策推進法！',
   },
 ];
